3b8f00e46e8a152697d2ada6a708d640d53e6318
sufianilyas <47196243+sufianilyas@users.noreply.github.com>
2021-03-25 09:37:43 -0400

    "Final Commit"

--- app/src/main/java/com/codpath/instaclone/Post.java
+++ app/src/main/java/com/codpath/instaclone/Post.java
@@ -14,6 +14,8 @@ public class Post extends ParseObject {
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_KEY = "createdAt";
+
 
 
     public String getDescription(){
